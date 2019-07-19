9e26873f1486115d9d450b66144cdc2e1324c2ef
HarrySpume <44293539+HarrySpume@users.noreply.github.com>
2019-07-19 19:06:44 +0800

    上传.Java文件
    第一次上传

---
+++ Demo.java
@@ -0,0 +1,23 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+
+/**
+ * BufferedOutputStream:处理流(高级流)，缓冲输出流
+ * @author Administrator
+ *
+ */
+public class Demo {
+    public static void main(String[] args){
+        try {
+            FileOutputStream fos=new FileOutputStream("src/FileSteam/复制黏贴.java");
+            BufferedOutputStream bos=new BufferedOutputStream(fos);
+            String content="我是缓冲输出流测试数据！";
+            bos.write(content.getBytes(),0,content.getBytes().length);
+            bos.flush();
+            bos.close();
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+    }
+}
+
